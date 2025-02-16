0106b30269b5dd034de4bd99fa794f2184df9e56
SxGhoul <sxghoul6@gmail.com>
2025-02-16 13:19:35 +0200

    first commit

--- Main.py
+++ Main.py
@@ -1,5 +1,10 @@
 import pygame
 
+def load_background_img(url):
+    maze = pygame.image.load(url).convert()
+    mask = pygame.mask.from_threshold(maze, (0, 0, 0, 255), (1, 1, 1, 255))
+    return maze, mask
+
 
 def main():
     pygame.init()
@@ -15,10 +20,21 @@ def main():
     # Скорость движения
     STEP = 10
 
+    maze, mask = load_background_img("Fon555566.png")
+
     # Параметры квадрата
     square_size = 30
     square_one_x = WIDTH // 10
     square_one_y = HEIGHT // 10
+
+    square_one_x = 10
+    square_one_y = 10
+    start_x, start_y =square_one_x, square_one_y
+
+    player_img = pygame.image.load("lolo.png")
+    player_img = pygame.transform.scale(player_img, (square_size, square_size))
+
+
 
     # Цвета
     WHITE = (255, 255, 255)
@@ -40,22 +56,18 @@ def main():
                     square_one_x += STEP
                 elif event.key == pygame.K_w:  # Движение вверх (W)
                     square_one_y -= STEP
-                elif event.key == pygame.K_s:  # Движение вниз (S)
+                elif event.key == pygame.K_s:
                     square_one_y += STEP
 
-        # Очистка экрана
-        screen.fill("darkgreen")
-
-        # Отрисовка квадрата
-        pygame.draw.rect(screen, KUBIC, (square_one_x, square_one_y, square_size, square_size))
-
-        # Обновление экрана
         pygame.display.flip()
 
-        # Ограничение FPS
+
+        screen.blit(maze, (0, 0))
+        screen.blit(player_img, (square_x, square_y))
+
         clock.tick(30)
 
-    pygame.quit()  # Теперь pygame.quit() внутри main()
+    pygame.quit()
 
 if __name__ == "__main__":
     main()
